aee92d06dbd14064506307e6e711914a9dc63297
Christopher Head <chead@chead.ca>
2023-06-25 13:41:59 -0700

    Port nbt-to-xml to libxml2

--- nbt/to_xml.cpp
+++ nbt/to_xml.cpp
@@ -5,12 +5,11 @@
 #include "util/globals.h"
 #include "util/mapped_file.h"
 #include "util/string.h"
+#include "util/xml.h"
 #include <cassert>
 #include <fcntl.h>
-#include <glibmm/ustring.h>
 #include <iostream>
-#include <libxml++/document.h>
-#include <libxml++/nodes/element.h>
+#include <libxml/tree.h>
 #include <limits>
 #include <locale>
 #include <sstream>
@@ -31,9 +30,9 @@ void eat(std::size_t n, const uint8_t *&input_ptr, std::size_t &input_left) {
 	input_left -= n;
 }
 
-void parse_name_and_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag, xmlpp::Element *parent_elt);
+void parse_name_and_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag, xmlNode &parent_elt);
 
-void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag, xmlpp::Element *parent_elt) {
+void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag, xmlNode &parent_elt) {
 	switch(tag) {
 		case nbt::TAG_END:
 			throw std::runtime_error("Malformed NBT: unexpected TAG_END.");
@@ -42,8 +41,8 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 			check_left(1, input_left);
 			int8_t value = codec::decode_u8(input_ptr);
 			eat(1, input_ptr, input_left);
-			xmlpp::Element *byte_elt = parent_elt->add_child(string::utf8_literal(u8"byte"));
-			byte_elt->set_attribute(string::utf8_literal(u8"value"), string::todecs(value));
+			xmlNode &byte_elt = xml::node_append_child(parent_elt, u8"byte");
+			xml::node_attr(byte_elt, u8"value", string::l2u(string::todecs_std(value)).c_str());
 			return;
 		}
 
@@ -51,8 +50,8 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 			check_left(2, input_left);
 			int16_t value = codec::decode_u16(input_ptr);
 			eat(2, input_ptr, input_left);
-			xmlpp::Element *short_elt = parent_elt->add_child(string::utf8_literal(u8"short"));
-			short_elt->set_attribute(string::utf8_literal(u8"value"), string::todecs(value));
+			xmlNode &short_elt = xml::node_append_child(parent_elt, u8"short");
+			xml::node_attr(short_elt, u8"value", string::l2u(string::todecs_std(value)).c_str());
 			return;
 		}
 
@@ -60,8 +59,8 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 			check_left(4, input_left);
 			int32_t value = codec::decode_u32(input_ptr);
 			eat(4, input_ptr, input_left);
-			xmlpp::Element *int_elt = parent_elt->add_child(string::utf8_literal(u8"int"));
-			int_elt->set_attribute(string::utf8_literal(u8"value"), string::todecs(value));
+			xmlNode &int_elt = xml::node_append_child(parent_elt, u8"int");
+			xml::node_attr(int_elt, u8"value", string::l2u(string::todecs_std(value)).c_str());
 			return;
 		}
 
@@ -69,8 +68,8 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 			check_left(8, input_left);
 			int64_t value = codec::decode_u64(input_ptr);
 			eat(8, input_ptr, input_left);
-			xmlpp::Element *long_elt = parent_elt->add_child(string::utf8_literal(u8"long"));
-			long_elt->set_attribute(string::utf8_literal(u8"value"), string::todecs(value));
+			xmlNode &long_elt = xml::node_append_child(parent_elt, u8"long");
+			xml::node_attr(long_elt, u8"value", string::l2u(string::todecs_std(value)).c_str());
 			return;
 		}
 
@@ -79,13 +78,13 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 			uint32_t raw = codec::decode_u32(input_ptr);
 			eat(4, input_ptr, input_left);
 			float fl = codec::decode_u32_to_float(raw);
-			xmlpp::Element *float_elt = parent_elt->add_child(string::utf8_literal(u8"float"));
-			std::wostringstream oss;
+			xmlNode &float_elt = xml::node_append_child(parent_elt, u8"float");
+			std::ostringstream oss;
 			oss.imbue(std::locale("C"));
 			oss.flags(std::ios_base::showpoint | std::ios_base::dec | std::ios_base::scientific | std::ios_base::left);
 			oss.precision(12);
 			oss << fl;
-			float_elt->set_attribute(string::utf8_literal(u8"value"), string::w2u(oss.str()));
+			xml::node_attr(float_elt, u8"value", string::l2u(oss.view()).c_str());
 			return;
 		}
 
@@ -94,13 +93,13 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 			uint64_t raw = codec::decode_u64(input_ptr);
 			eat(8, input_ptr, input_left);
 			double db = codec::decode_u64_to_double(raw);
-			xmlpp::Element *double_elt = parent_elt->add_child(string::utf8_literal(u8"double"));
-			std::wostringstream oss;
+			xmlNode &double_elt = xml::node_append_child(parent_elt, u8"double");
+			std::ostringstream oss;
 			oss.imbue(std::locale("C"));
 			oss.flags(std::ios_base::showpoint | std::ios_base::dec | std::ios_base::scientific | std::ios_base::left);
 			oss.precision(20);
 			oss << db;
-			double_elt->set_attribute(string::utf8_literal(u8"value"), string::w2u(oss.str()));
+			xml::node_attr(double_elt, u8"value", string::l2u(oss.view()).c_str());
 			return;
 		}
 
@@ -112,22 +111,22 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 				throw std::runtime_error("Malformed NBT: negative byte array length.");
 			}
 			check_left(len, input_left);
-			xmlpp::Element *barray_elt = parent_elt->add_child(string::utf8_literal(u8"barray"));
-			Glib::ustring ustr;
+			xmlNode &barray_elt = xml::node_append_child(parent_elt, u8"barray");
+			std::u8string ustr;
 			ustr.reserve(1 + len * 2 + len / 50 + 1);
-			ustr.append(string::utf8_literal(u8"\n"));
+			ustr.push_back(u8'\n');
 			for(int32_t i = 0; i < len; ++i) {
 				static const char8_t DIGITS[] = u8"0123456789ABCDEF";
 				uint8_t byte = input_ptr[i];
-				ustr.push_back(static_cast<char>(DIGITS[byte >> 4]));
-				ustr.push_back(static_cast<char>(DIGITS[byte & 0x0F]));
+				ustr.push_back(DIGITS[byte >> 4]);
+				ustr.push_back(DIGITS[byte & 0x0F]);
 				if((i % 50) == 49) {
-					ustr.append(string::utf8_literal(u8"\n"));
+					ustr.push_back(u8'\n');
 				}
 			}
 			eat(len, input_ptr, input_left);
-			ustr.append(string::utf8_literal(u8"\n"));
-			barray_elt->add_child_text(ustr);
+			ustr.push_back(u8'\n');
+			xml::node_append_text(barray_elt, ustr);
 			return;
 		}
 
@@ -138,8 +137,9 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 			if(len < 0) {
 				throw std::runtime_error("Malformed NBT: negative string length.");
 			}
-			xmlpp::Element *string_elt = parent_elt->add_child(string::utf8_literal(u8"string"));
-			string_elt->set_attribute(string::utf8_literal(u8"value"), Glib::ustring(std::string(input_ptr, input_ptr + len)));
+			xmlNode &string_elt = xml::node_append_child(parent_elt, u8"string");
+			std::u8string value(input_ptr, input_ptr + len);
+			xml::node_attr(string_elt, u8"value", value.c_str());
 			eat(len, input_ptr, input_left);
 			return;
 		}
@@ -153,8 +153,8 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 			if(len < 0) {
 				throw std::runtime_error("Malformed NBT: negative list length.");
 			}
-			xmlpp::Element *list_elt = parent_elt->add_child(string::utf8_literal(u8"list"));
-			list_elt->set_attribute(string::utf8_literal(u8"subtype"), string::todecu(subtype));
+			xmlNode &list_elt = xml::node_append_child(parent_elt, u8"list");
+			xml::node_attr(list_elt, u8"subtype", string::l2u(string::todecu_std(subtype)).c_str());
 			for(int32_t i = 0; i < len; ++i) {
 				parse_data(input_ptr, input_left, subtype, list_elt);
 			}
@@ -162,7 +162,7 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 		}
 
 		case nbt::TAG_COMPOUND: {
-			xmlpp::Element *compound_elt = parent_elt->add_child(string::utf8_literal(u8"compound"));
+			xmlNode &compound_elt = xml::node_append_child(parent_elt, u8"compound");
 			for(;;) {
 				check_left(1, input_left);
 				nbt::tag subtype = static_cast<nbt::tag>(codec::decode_u8(input_ptr));
@@ -187,23 +187,23 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 				throw std::runtime_error("Unsupported NBT feature: integer array length too big.");
 			}
 			check_left(len, input_left * 4);
-			xmlpp::Element *iarray_elt = parent_elt->add_child(string::utf8_literal(u8"iarray"));
-			Glib::ustring ustr;
+			xmlNode &iarray_elt = xml::node_append_child(parent_elt, u8"iarray");
+			std::u8string ustr;
 			ustr.reserve(1 + len * 8 + len / 10 + 1);
-			ustr.append(string::utf8_literal(u8"\n"));
+			ustr.push_back(u8'\n');
 			for(int32_t i = 0; i < len; ++i) {
 				static const char8_t DIGITS[] = u8"0123456789ABCDEF";
 				uint32_t integer = codec::decode_u32(input_ptr + i * 4);
 				for(int nybble = 7; nybble >= 0; --nybble) {
-					ustr.push_back(static_cast<char>(DIGITS[(integer >> (4 * nybble)) & 0x0F]));
+					ustr.push_back(DIGITS[(integer >> (4 * nybble)) & 0x0F]);
 				}
 				if((i % 10) == 9) {
-					ustr.append(string::utf8_literal(u8"\n"));
+					ustr.push_back(u8'\n');
 				}
 			}
 			eat(len * 4, input_ptr, input_left);
-			ustr.append(string::utf8_literal(u8"\n"));
-			iarray_elt->add_child_text(ustr);
+			ustr.push_back(u8'\n');
+			xml::node_append_text(iarray_elt, ustr);
 			return;
 		}
 
@@ -218,23 +218,23 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 				throw std::runtime_error("Unsupported NBT feature: long array length too big.");
 			}
 			check_left(len, input_left * 8);
-			xmlpp::Element *larray_elt = parent_elt->add_child(string::utf8_literal(u8"larray"));
-			Glib::ustring ustr;
+			xmlNode &larray_elt = xml::node_append_child(parent_elt, u8"larray");
+			std::u8string ustr;
 			ustr.reserve(1 + len * 16 + len / 10 + 1);
-			ustr.append(string::utf8_literal(u8"\n"));
+			ustr.push_back(u8'\n');
 			for(int32_t i = 0; i < len; ++i) {
 				static const char8_t DIGITS[] = u8"0123456789ABCDEF";
 				uint64_t integer = codec::decode_u64(input_ptr + i * 8);
 				for(int nybble = 15; nybble >= 0; --nybble) {
-					ustr.push_back(static_cast<char>(DIGITS[(integer >> (4 * nybble)) & 0x0F]));
+					ustr.push_back(DIGITS[(integer >> (4 * nybble)) & 0x0F]);
 				}
 				if((i % 10) == 9) {
-					ustr.append(string::utf8_literal(u8"\n"));
+					ustr.push_back(u8'\n');
 				}
 			}
 			eat(len * 8, input_ptr, input_left);
-			ustr.append(string::utf8_literal(u8"\n"));
-			larray_elt->add_child_text(ustr);
+			ustr.push_back(u8'\n');
+			xml::node_append_text(larray_elt, ustr);
 			return;
 		}
 	}
@@ -242,7 +242,7 @@ void parse_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag
 	throw std::runtime_error("Malformed NBT: unrecognized tag.");
 }
 
-void parse_name_and_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag, xmlpp::Element *parent_elt) {
+void parse_name_and_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt::tag tag, xmlNode &parent_elt) {
 	check_left(2, input_left);
 	int16_t name_length = codec::decode_u16(input_ptr);
 	eat(2, input_ptr, input_left);
@@ -250,10 +250,10 @@ void parse_name_and_data(const uint8_t *&input_ptr, std::size_t &input_left, nbt
 		throw std::runtime_error("Malformed NBT: negative element name length.");
 	}
 	check_left(name_length, input_left);
-	Glib::ustring name(std::string(input_ptr, input_ptr + name_length));
+	std::u8string name(input_ptr, input_ptr + name_length);
 	eat(name_length, input_ptr, input_left);
-	xmlpp::Element *named_elt = parent_elt->add_child(string::utf8_literal(u8"named"));
-	named_elt->set_attribute(string::utf8_literal(u8"name"), name);
+	xmlNode &named_elt = xml::node_append_child(parent_elt, u8"named");
+	xml::node_attr(named_elt, u8"name", name.c_str());
 	parse_data(input_ptr, input_left, tag, named_elt);
 }
 }
@@ -278,9 +278,9 @@ int mcwutil::nbt::to_xml(std::ranges::subrange<char **> args) {
 	mapped_file input_mapped(input_fd, PROT_READ);
 
 	// Construct document.
-	xmlpp::Document nbt_document;
-	nbt_document.set_internal_subset(string::utf8_literal(u8"minecraft-nbt"), string::utf8_literal(u8""), string::utf8_literal(u8"urn:uuid:25323dd6-2a7d-11e1-96b7-1c4bd68d068e"));
-	xmlpp::Element *nbt_root_elt = nbt_document.create_root_node(string::utf8_literal(u8"minecraft-nbt"));
+	auto nbt_document = xml::empty();
+	xml::internal_subset(*nbt_document, u8"minecraft-nbt", nullptr, u8"urn:uuid:25323dd6-2a7d-11e1-96b7-1c4bd68d068e");
+	xmlNode &nbt_root_elt = xml::node_create_root(*nbt_document, u8"minecraft-nbt");
 	const uint8_t *input_ptr = static_cast<const uint8_t *>(input_mapped.data());
 	std::size_t input_left = input_mapped.size();
 	check_left(1, input_left);
@@ -289,7 +289,8 @@ int mcwutil::nbt::to_xml(std::ranges::subrange<char **> args) {
 	parse_name_and_data(input_ptr, input_left, outer_tag, nbt_root_elt);
 
 	// Write output file.
-	nbt_document.write_to_file_formatted(args[1]);
+	file_descriptor nbt_fd = file_descriptor::create_open(args[1], O_WRONLY | O_CREAT, 0666);
+	xml::write(*nbt_document, nbt_fd);
 
 	return 0;
 }
